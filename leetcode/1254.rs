caeebd263969120a51c20d39969e4bb755ba8056
Devesh Sharma <50168780+DaveyDark@users.noreply.github.com>
2023-04-07 23:14:19 +0530

    Number of Enclaves

--- leetcode/1254.rs
+++ leetcode/1254.rs
@@ -1,23 +1,23 @@
 /*
 Question:
-Given a 2D grid consists of 0s (land) and 1s (water).  
-An island is a maximal 4-directionally connected group of 0s 
-and a closed island is an island totally (all left, top, right, bottom) surrounded by 1s.
+You are given an m x n binary matrix grid, where 0 represents a sea cell and 1 represents a land cell.
 
-Return the number of closed islands.
+A move consists of walking from one land cell to another adjacent (4-directionally) land cell or walking off the boundary of the grid.
+
+Return the number of land cells in grid for which we cannot walk off the boundary of the grid in any number of moves.
 */
 
 // Approach:
 // 1) Loop through all middle elements of the grid
 // 2) If there is land at any element, then we call the terraform function on it
-//    Terraform function recursively checks all connected land tiles and returns true if it's an island
+//    Terraform function recursively checks all connected land tiles and returns counts the land tiles
+//    Terraform changes the counter to -1 if the lang chunk is connected to the edge
 //    Terraform also changes the 0s to 2s so it doesn't get into an infinite loop and each island
 //    Only gets terraformed once
-// 3) If the terraform returns true, we increase the counter
+// 3) If the terraform returns > 0, we increase the counter by the returned value
 
-fn terraform(grid: &mut Vec<Vec<i32>>, y: usize, x: usize) -> bool{
+fn terraform(grid: &mut Vec<Vec<i32>>, y: usize, x: usize, cnt: &mut i32) {
     grid[y][x] = 2;
-    let mut ret = true;
     for i in -1..=1i32{
         for j in -1..=1i32{
             if !((i == 0)^(j == 0)) {
@@ -25,33 +25,35 @@ fn terraform(grid: &mut Vec<Vec<i32>>, y: usize, x: usize) -> bool{
             }
             if (y as i32 + i) < 0 || (x as i32 + j) < 0 || (y + i as usize) >= grid.len() || (x + j as usize) >= grid[0].len() {
                 //edge touching element
-                ret = false;
+                *cnt = -1;
                 continue;
             }
-            if grid[y+i as usize][x+j as usize] == 0 {
+            if grid[y+i as usize][x+j as usize] == 1 {
                 //adjacent land
-                if !terraform(grid,y+i as usize,x+j as usize){
-                    //if it's touching edge
-                    ret = false;
+                if *cnt >= 0 {
+                    *cnt += 1;
+                    println!("land at {} {}",y+i as usize,x+j as usize);
                 }
+                terraform(grid,y+i as usize,x+j as usize,cnt);
             }
         }
     }
-    ret
 }
 impl Solution {
-    pub fn closed_island(grid: Vec<Vec<i32>>) -> i32 {
+    pub fn num_enclaves(grid: Vec<Vec<i32>>) -> i32 {
         let mut grid = grid.clone();
-        let mut islands = 0;
+        let mut enclaves = 0;
         for i in 1..grid.len()-1 {
             for j in 1..grid[0].len()-1 {
-                if grid[i][j] == 0 {
-                    if terraform(&mut grid,i,j) {
-                        islands+=1;
+                if grid[i][j] == 1 {
+                    let mut cnt: i32 = 1;
+                    terraform(&mut grid,i,j,&mut cnt);
+                    if cnt > 0 {
+                        enclaves += cnt;
                     }
                 }
             }
         }
-        islands
+        enclaves
     }
 }
