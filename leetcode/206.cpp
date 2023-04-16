5253f9e7832f1eba55497a16ae9e66982a4d46b1
Devesh Sharma <50168780+DaveyDark@users.noreply.github.com>
2023-04-16 21:40:16 +0530

    Reverse Linked List

---
+++ leetcode/206.cpp
@@ -0,0 +1,43 @@
+/*
+Given the head of a singly linked list, reverse the list, and return the reversed list.
+*/
+
+// Approach:
+// 1) We create a stack and push copies of all nodes in the list into it
+// 2) We pop out nodes from the stack one by one until the stack is empty
+//    The previous node is tracked and assigned the 'next' value of the current node
+// 3) The next of the last node is set to null and the head is returned
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+
+#include <stack>
+
+class Solution {
+public:
+    ListNode* reverseList(ListNode* head) {
+        std::stack<ListNode*> stack;
+        while(head){
+            stack.push(new ListNode(head->val,head->next));
+            head = head->next;
+        }
+        if(stack.empty())return nullptr;
+        head = stack.top();
+        ListNode *prevNode = stack.top(), *node;
+        for(;!stack.empty();stack.pop()){
+            node = stack.top();
+            prevNode->next = node;
+            prevNode = node;
+        }
+        node->next = nullptr;
+        return head;
+    }
+};
