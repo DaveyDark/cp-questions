78185466563a322be3e7d84d8fadb4d756e87dce
Devesh Sharma <50168780+DaveyDark@users.noreply.github.com>
2023-05-05 12:11:18 +0530

    Number of 1 Bits

---
+++ leetcode/191.rs
@@ -0,0 +1,18 @@
+/*
+Question:
+Write a function that takes the binary representation of an unsigned integer and returns the number of '1' bits it has (also known as the Hamming weight).
+Note:
+    Note that in some languages, such as Java, there is no unsigned integer type. In this case, the input will be given as a signed integer type. It should not affect your implementation, as the integer's internal binary representation is the same, whether it is signed or unsigned.
+    In Java, the compiler represents the signed integers using 2's complement notation. Therefore, in Example 3, the input represents the signed integer. -3.
+*/
+
+// Approach:
+// 1) We conver the number to a binary string
+// 2) Then we filter it to keep only 1 bits
+// 3) Then we count and return the number of elements
+
+impl Solution {
+    pub fn hammingWeight (n: u32) -> i32 {
+        format!("{:b}",n).chars().filter(|x| *x == '1').count() as i32
+    }
+}
