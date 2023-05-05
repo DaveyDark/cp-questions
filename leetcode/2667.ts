3a9b66165001d4f1ecb357daba95f04e8c45000a
Devesh Sharma <50168780+DaveyDark@users.noreply.github.com>
2023-05-06 01:08:28 +0530

    Create Hello World Function

---
+++ leetcode/2667.ts
@@ -0,0 +1,10 @@
+/*
+Question:
+Write a function createHelloWorld. It should return a new function that always returns "Hello World". 
+*/
+
+function createHelloWorld() {
+	return function(...args): string {
+        return "Hello World";
+    };
+};
