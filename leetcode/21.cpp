3a51f105fe2c5b0c1c92f49cdfab011ab8468ef9
Devesh Sharma <50168780+DaveyDark@users.noreply.github.com>
2023-05-05 12:43:58 +0530

    Merge Two Sorted Lists

--- leetcode/21.cpp
+++ leetcode/21.cpp
@@ -21,6 +21,7 @@ Return the head of the merged linked list.
  *     ListNode(int x, ListNode *next) : val(x), next(next) {}
  * };
  */
+ // Iterative Approach
 class Solution {
 public:
     ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
@@ -43,4 +44,22 @@ public:
         }
         return start->next;
     }
+};
+
+// Recursive Approach
+class Solution {
+public:
+	ListNode* mergeTwoLists(ListNode* l1, ListNode* l2) 
+  {
+		if(!l1)return l2;
+		if(!l2)return l1;
+		
+		if(l1 -> val <= l2 -> val) {
+			l1->next = mergeTwoLists(l1 -> next, l2);
+			return l1;
+		} else {
+			l2->next = mergeTwoLists(l1, l2 -> next);
+			return l2;            
+		}
+	}
 };
