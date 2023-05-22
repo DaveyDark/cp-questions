5fd9b9e72e4210156877740dd0e4424955bd21e6
Devesh Sharma <50168780+DaveyDark@users.noreply.github.com>
2023-05-22 15:22:46 +0530

    Find the Index of the First Occurrence in a String

---
+++ leetcode/28.java
@@ -0,0 +1,13 @@
+/*
+Question:
+Given two strings needle and haystack, return the index of the first occurrence of needle in haystack, or -1 if needle is not part of haystack.
+*/
+
+// Approach:
+// Self-Explainatory
+
+class Solution {
+    public int strStr(String haystack, String needle) {
+        return haystack.indexOf(needle);
+    }
+}
