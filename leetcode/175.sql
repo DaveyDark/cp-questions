6c4f67de35dd53b241d5790a28e2cc90db120d6d
Devesh Sharma <50168780+DaveyDark@users.noreply.github.com>
2023-07-30 14:23:06 +0530

    Combine Two Tables

---
+++ leetcode/175.sql
@@ -0,0 +1,8 @@
+-- Question:
+-- Write an SQL query to report the first name, last name, city, and state of each person in the Person table. 
+-- If the address of a personId is not present in the Address table, report null instead.
+-- Return the result table in any order.
+
+SELECT firstName, lastName, city, state 
+FROM Person 
+LEFT JOIN Address ON Person.personId = Address.personId;
